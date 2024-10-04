dd5275c40f4e24040e76b2388f13265f050e4ea2
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-04 23:15:38 +0530

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,8 +1,25 @@
 import React from 'react'
+import { useEffect } from 'react'
 
 export default function Signin() {
   return (
     <div className='signinbox'>
+        <div className='inputs' id='signup'>
+            <h1>Sign in</h1>
+            <h3>Email</h3>
+            <input type="text" className='credentials'/>
+            <h3>password</h3>
+            <input type="password" className='credentials' />
+            <button>Sign in</button>
+            <div className='helpsignin'>
+                <a href="">Forgot password</a>
+                <div className='remember'>
+                    <p>Remember me?</p>
+                    <input type="checkbox" />
+                </div>
+                
+            </div>
+        </div>
         <div className='brandbox'>
             <h1>Welcome to</h1>
             <div className='brandlogo'>
@@ -11,23 +28,23 @@ export default function Signin() {
             </div>
             <span className='branddisc'>Buy anything and everything</span>
             <p>don't have an account?</p>
-            <button>Sign up</button>
+            <button id='signbtn'>Sign up</button>
         </div>
-        <div className='inputs'>
-        <h1>Sign in</h1>
-        <h3>Email</h3>
-        <input type="text" />
-        <h3>password</h3>
-        <input type="password" />
-        <button>Sign in</button>
-        <div>
-            <a href="">Forgot</a>
-            <div>
-                <p>Remember me?</p>
-                <input type="checkbox" />
+        <div className='inputs' id='signin'>
+            <h1>Sign in</h1>
+            <h3>Email</h3>
+            <input type="text" className='credentials'/>
+            <h3>password</h3>
+            <input type="password" className='credentials' />
+            <button>Sign in</button>
+            <div className='helpsignin'>
+                <a href="">Forgot password</a>
+                <div className='remember'>
+                    <p>Remember me?</p>
+                    <input type="checkbox" />
+                </div>
+                
             </div>
-            
-        </div>
         </div>
     </div>
   )
