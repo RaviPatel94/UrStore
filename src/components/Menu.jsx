512a6a6e2dde8e24a9d8610d9b6a8c2d37f1e854
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-04 23:10:14 +0530

    Update Menu.jsx for json

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -1,121 +1,18 @@
 import React from 'react'
+import Product from './Product'
+import items from './item.json';
 
 export const Menu=()=> {
   return (
     <div className='menu'>
-        <div className="itemdispay">
-            <div> <img className='itemthumbnail' src="src\images\shoes.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Air walk 26th edition</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div>
-        </div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\perfume.avif" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Fogg perfume</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\lamp.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>lamp 2pc</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\mousekey.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Mouse & Keyboard</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\blackearbud.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Air walk 26th edition</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\blackwiredearphone.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Air walk 26th edition</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\mensring.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Air walk 26th edition</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div>
-        <div className="itemdispay">            
-            <div> <img className='itemthumbnail' src="src\images\shoes.jpg" alt="shoes" /></div>
-            <div>
-                <div className='itemtitle'>Air walk 26th edition</div>
-                <div className='revcost'>
-                    <div className='review'>
-                        <div className='starno'>4.6</div>
-                        <div>
-                            <img className="star" src="src\images\star.png" alt="" />
-                        </div>
-                    </div>
-                    <div className='mcost'>5 P</div>
-                </div>
-            </div></div> 
+        {items.map((item) => (
+          <Product
+            title={item.title}
+            image={item.image}
+            rating={item.rating}
+            cost={item.cost}
+          />
+        ))}
     </div>
   )
 }
