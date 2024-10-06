6af3bf011f6ac14e8242f77b3b91535bb186d736
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-06 09:11:13 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,12 +4,14 @@ import { Menu } from './components/Menu'
 import './App.css'
 import Footer from './components/Footer'
 import Signin from './components/Signin'
+import Navbar from './components/navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className='whole'>
+      <Navbar/>
       <Herobanner />
       <Menu/>
       <Footer/>
