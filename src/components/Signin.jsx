1999ee57e8967a821a1cce6f348e8312c810d48e
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-08 21:49:27 +0530

    Update Signin.jsx

--- src/components/Signin.jsx
+++ src/components/Signin.jsx
@@ -1,42 +1,58 @@
 import React from 'react'
-import { useEffect } from 'react'
+import { useEffect , useState , useCallback} from 'react'
+import { Link } from 'react-router-dom'
 
 export default function Signin() {
+    const [signbtn , btnClicked]=useState(false)
+    const [value1, setValue1]=useState("flex")
+    const [value2,setValue2]=useState("none")
+    const [btnlbl,setbtn]=useState("Signup")
+    const [dont,setdont]=useState("Don't")
+    const Change=()=>{
+        setValue1((prevDisplay) => (prevDisplay === 'none' ? 'flex' : 'none'));
+        setValue2((prevDisplay) => (prevDisplay === 'none' ? 'flex' : 'none'));
+        setbtn((prevDisplay) => (prevDisplay === 'Signup' ? 'Signin' : 'Signup'));
+        setdont((prevDisplay) => (prevDisplay === "Don't" ? '' : "Don't"));
+        console.log(value1,value2)
+    }
   return (
-    <div className='signinbox'>
-        <div className='inputs' id='signup'>
-            <h1>Sign in</h1>
+    <div className='signinbox'> 
+        <div className='inputs' id='signup' style={{display:value2}
+            }>
+            <h1>Sign Up</h1>
+            <h3>Name</h3>
+            <input type="text" className='credentials' />
             <h3>Email</h3>
-            <input type="text" className='credentials'/>
+            <input type="email" className='credentials'/>
             <h3>password</h3>
             <input type="password" className='credentials' />
-            <button>Sign in</button>
-            <div className='helpsignin'>
-                <a href="">Forgot password</a>
-                <div className='remember'>
-                    <p>Remember me?</p>
-                    <input type="checkbox" />
-                </div>
-                
-            </div>
+            <button>
+                <Link to="/questions" className='navlink'>
+                Sign Up
+                </Link>
+            </button>
         </div>
         <div className='brandbox'>
             <h1>Welcome to</h1>
             <div className='brandlogo'>
-                <img src='src\images\logo.png'/>
+                <img src='\images\logo.png'/>
                 <span className='logotext'>UrStore</span>
             </div>
             <span className='branddisc'>Buy anything and everything</span>
-            <p>don't have an account?</p>
-            <button id='signbtn'>Sign up</button>
+            <p>{dont} have an account?</p>
+            <button id='signbtn' onClick={() => {btnClicked((prev) => !prev); Change(); }}>{btnlbl}</button>
         </div>
-        <div className='inputs' id='signin'>
+        <div className='inputs' id='signin' style={{display:value1}}>
             <h1>Sign in</h1>
             <h3>Email</h3>
             <input type="text" className='credentials'/>
             <h3>password</h3>
             <input type="password" className='credentials' />
-            <button>Sign in</button>
+            <button>
+                <Link to="/questions" className='navlink'>
+                Sign in
+                </Link>
+            </button>
             <div className='helpsignin'>
                 <a href="">Forgot password</a>
                 <div className='remember'>
