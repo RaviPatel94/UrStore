b5329324afd0f18af5019e6b57462846dde38aa1
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-08 21:46:15 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,9 +1,10 @@
 import React from "react";
-import logo from "../images/logo.png";
-import heart from "../images/heart.png";
-import contact from "../images/contact.png";
-import trolley from "../images/cart1.png";
-import user from "../images/icon.jpg";
+import logo from "/images/logo.png";
+import heart from "/images/heart.png";
+import contact from "/images/contact.png";
+import trolley from "/images/cart1.png";
+import user from "/images/icon.jpg";
+import {Link, NavLink} from 'react-router-dom'
 export default function Navbar() {
   return (
     <>
@@ -25,13 +26,20 @@ export default function Navbar() {
           <img className="icon" src={heart} alt="Favorites" />
           <img className="icon" src={contact} alt="Contact" />
           <img className="icon" src={trolley} alt="Cart" />
-          <div className="pfpdiv"><img className="icon" src={user} alt="User" />
+          <div className="pfpdiv"><img className="iconpfp" src={user} alt="User" />
             <ul className="dropdown-menu">
-              <li>Login</li>
+              <li>
+                <NavLink className="navlink"
+                to={"/login"}>
+                  Login
+                </NavLink>
+              </li>
+              <hr />
               <li>Team</li>
+              <hr />
               <li>Help</li>
+              <hr />
               <li>Report</li>
-              <li>Theme</li>
             </ul>
           </div>
           </div>
