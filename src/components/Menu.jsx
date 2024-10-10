cfd967c6759537a8b54bb0c6268527b95272e079
snYatharth2005 <139547990+snYatharth2005@users.noreply.github.com>
2024-10-10 09:40:41 +0530

    Update Menu.jsx

--- src/components/Menu.jsx
+++ src/components/Menu.jsx
@@ -4,15 +4,18 @@ import items from './item.json';
 
 export const Menu=()=> {
   return (
+    <>
     <div className='menu'>
         {items.map((item) => (
           <Product
-            title={item.title}
+          id = {item.id}
+          title={item.title}
             image={item.image}
             rating={item.rating}
             cost={item.cost}
-          />
-        ))}
+            />
+          ))}
     </div>
+    </>
   )
 }
