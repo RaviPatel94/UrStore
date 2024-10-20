95b1e091b8c459b1b8f0885270f4a163594b4464
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-20 13:03:50 +0530

    Update navbar.jsx

--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,64 +1,113 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import logo from "/images/logo.png";
 import heart from "/images/heart.png";
 import contact from "/images/contact.png";
 import trolley from "/images/cart1.png";
 import user from "/images/icon.jpg";
-import {Link, NavLink} from 'react-router-dom'
+import { Link, NavLink } from "react-router-dom";
+import { useSelector } from "react-redux";
+import CartTab from "./CartTab";
 export default function Navbar() {
+  const [totalQuantity, setTotalQuantity] = useState(0);
+  const carts = useSelector(store => store.cart.items );
+  useEffect(() => {
+    let total = 0;
+    carts.forEach(item => total += item.quantity);
+    setTotalQuantity(total);
+  }, [carts])
   return (
     <>
-      <nav className="navbar" >
+      <nav className="navbar">
         <div id="mainnav">
-        <div className="navleft" >
-        <div className="logo">
-          <img src={logo} alt="Logo" className="navbar-logo" />
-          <h1 className="logo-name">UrStore</h1>
-        </div>
-        <input className="search-bar" type="text" placeholder="Search" />
-        </div>
-        <div className="navbar-right">
-          <div className="icon-container">
-            <span className="pointlogo">P</span>
-            <span className="pointsdisplay">100</span>
+          <div className="navleft">
+            <div className="logo">
+              <img src={logo} alt="Logo" className="navbar-logo" />
+              <h1 className="logo-name">UrStore</h1>
+            </div>
+            <input className="search-bar" type="text" placeholder="Search" />
           </div>
-          <div className="options">
-          <img className="icon" src={heart} alt="Favorites" />
-          <img className="icon" src={contact} alt="Contact" />
-          <img className="icon" src={trolley} alt="Cart" />
-          <div className="pfpdiv"><img className="iconpfp" src={user} alt="User" />
-            <ul className="dropdown-menu">
-            <NavLink className="navlink"
-              to={"/login"}>
-              <li>
-                  Login
-              </li>
-              </NavLink>
-              <hr />
-              <li>Team</li>
-              <hr />
-              <li>Help</li>
-              <hr />
-              <li>Report</li>
-            </ul>
+          <div className="navbar-right">
+            <div className="icon-container">
+              <span className="pointlogo">P</span>
+              <span className="pointsdisplay">100</span>
+            </div>
+            <div className="options">
+              <img
+                className="icon"
+                style={{ margin: "5px" }}
+                src={heart}
+                alt="Favorites"
+              />
+              <img
+                className="icon"
+                style={{ margin: "6px" }}
+                src={contact}
+                alt="Contact"
+              />
+                <div
+                  style={{
+                    position: "relative",
+                    display: "inline-block",
+                    margin: "5px",
+                  }}
+                  className="cart-div"
+                >
+                  <img className="icon" src={trolley} alt="Cart" />
+
+                  
+                  <span
+                    style={{
+                      position: "absolute",
+                      top: "-5px",
+                      right: "-5px",
+                      backgroundColor: "red",
+                      color: "white",
+                      borderRadius: "50%",
+                      width: "15px",
+                      height: "15px",
+                      display: "inline-block",
+                      textAlign: "center",
+                      lineHeight: "15px",
+                      fontWeight: "bold",
+                      textDecoration: "none",
+                      fontSize: "10px", 
+                    }}
+                  > {totalQuantity} </span>
+                  <ul className="dropdown-menu-cart">
+                    <CartTab/>
+                </ul>
+                </div>
+              <div className="pfpdiv">
+                <img className="iconpfp" src={user} alt="User" />
+                <ul className="dropdown-menu">
+                  <NavLink className="navlink" to={"/login"}>
+                    <li>Login</li>
+                  </NavLink>
+                  <hr />
+                  <li>Team</li>
+                  <hr />
+                  <li>Help</li>
+                  <hr />
+                  <li>Report</li>
+                </ul>
+              </div>
+            </div>
           </div>
-          </div>
-        </div>
         </div>
       </nav>
       <nav className="navbar">
         <div className="navbar-left">
           <div className="navbar-links">
-            <a href="/" className="category">Home</a>
-            <a href="/clothes" className="category">Clothes</a>
-            <a href="/electronics" className="category">Electronics</a>
-            <a href="/kids" className="category">Kids</a>
-            <a href="/students" className="category">Students</a>
-            <a href="/furniture" className="category">Furniture</a>
-            <a href="/gifts" className="category">Gifts</a>
-            <a href="/sell" className="category">Sell</a>
-            <a href="/accessories" className="category">Accessories</a>
-            <a href="/bitcoin" className="category">Bitcoin</a>
+            <NavLink to='/home'> Home</NavLink>
+            <NavLink to="/clothes">Clothes</NavLink>
+            <NavLink to="/electronics">Electronics</NavLink>
+            <NavLink to="/kids">Kids</NavLink>
+            <NavLink to="/students">Students</NavLink>
+            <NavLink to="/furniture">Furniture</NavLink>
+            <NavLink to="/gifts">Gifts</NavLink>
+            <NavLink to="/sell">Sell</NavLink>
+            <NavLink to="/accessories">Accessories</NavLink>
+            <NavLink to="/bitcoin">Bitcoin</NavLink>
           </div>
         </div>
       </nav>
