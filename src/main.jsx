fb4beabad5515b556506d41a68d1b5a9481bee29
Ravi Patel <139551180+RaviPatel94@users.noreply.github.com>
2024-10-20 12:33:53 +0530

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -11,7 +11,7 @@ import { Menu } from './components/Menu'
 import Questions from './components/Questions.jsx'
 import { Herobanner } from './components/Herobanner'
 import Description from './components/ProductDescription.jsx'
-import { Provider } from 'react-redux';
+import { Provider } from 'react-redux'
 import {store} from './stores'
 import CartTab from './components/cartTab.jsx'
 import CheckOut from './components/CheckOut.jsx'
