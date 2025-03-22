7687ca31c182f2c33e80770c5279f66a445c072f
RaviPatel94 <ravi942004@gmail.com>
2025-03-22 10:03:51 +0530

    connect db

---
+++ server/connect.cjs
@@ -0,0 +1,7 @@
+const {MongoClient} = require("mongodb")
+require("dotenv").config({path: "./config.env"})
+
+async function main(){
+    const Db = process.env.ATLAS_URI
+    const client = new MongoClient(Db)
+}
